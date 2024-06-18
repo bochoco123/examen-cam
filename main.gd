add388cf98792894df3b579a0399a23c16ade6c6
unknown <camila.itzxl01@gmail.com>
2024-06-18 13:05:52 -0600

    kkk

--- main.gd
+++ main.gd
@@ -1,39 +1,101 @@
 extends CanvasLayer
-var is_timer
-var segundos = 55
+var is_timer = true
+var minutes = 0
+var seconds = 0
+var decimals = 0
+var mode_label = 'Timer'
 
-var minutos = 0 
-var decimales = 0
-var formated_time = ''
+func update_label():
+	$ClLabel.text = str(decimals)
+	$TimeLabel.text = '%02d:%02d'%[minutes,seconds]
+	
 
+			
+		
+func update_stopwatch():
+	decimals += 1
+	if decimals >= 9:
+		decimals = 0
+		seconds += 1
+	if minutes >= 59:
+		seconds = 59
+		minutes = 59
+		decimals = 9
+		$StatusLabel.text = 'Stopped'
+		$Timer.stop()
+	update_label()
+		
 func update_timer():
-	decimales = decimales + 1
-	
-	if decimales > 9:
-		segundos = segundos + 1
-		decimales = 0
-	if segundos > 59:
-		minutos = minutos +1
-		segundos = 0
-		decimales = 0
-	if minutos > 59:
-		minutos = minutos + 1
-	
-	
-	formated_time = '%02d:%02d:%02d'%[decimales,segundos,minutos]
-	$Label.text = formated_time
-	
-func _on_button_pressed():
-	$Timer.start()
-func _on_timer_timeout():
-	update_timer()
-func _on_stop_pressed():
-	if $Timer.stop()
-	
-()
-	%Timer.restart()
-func _on_restart_pressed():
-	segundos = 0
-	minutos = 0
-	segundos = 0
-	$Timer.start()
+	decimals -= 1
+	if decimals < 0:
+		decimals = 9
+		seconds -= 1
+	if seconds <0:
+		seconds = 59
+		minutes -= 1
+	if minutes < 0:
+		minutes = 0
+		seconds = 0
+		decimals = 0
+		$StatusLabel.text = 'Stopped'
+		$Timer.stop()
+	update_label()
+
+func _on_main_timer_timeout():
+	if is_timer == false:
+		update_stopwatch()
+	else:
+		update_timer()
+		
+func _on_seco_button_pressed():
+	if $Timer.is_stopped() and is_timer == true:
+		seconds += 1
+		if seconds > 59:
+			seconds = 0
+		decimals = 0
+		update_label()
+			
+
+		
+func _on_clear_button_pressed():
+	if $Timer.is_stopped():
+		minutes = 0
+		seconds = 0
+		decimals = 0
+		update_label()
+			
+			
+			
+			
+
+
+func _on_mode_button_pressed():
+	if $Timer.is_stopped():
+		minutes = 0
+		seconds = 0
+		decimals = 0
+		is_timer = is_timer
+		if is_timer == true:
+			mode_label = 'Timer'
+		else :
+			mode_label = 'Stopwatch'
+			update_label()
+			%ModeLabel.text = mode_label
+
+
+func _on_minu_button_pressed():
+	if $Timer.is_stopped() and is_timer == true:
+		minutes += 1
+		if minutes > 59:
+			minutes = 0
+		decimals = 0 
+		update_label()
+		
+func _on_start_button_pressed():
+	if $Timer.is_stopped():
+		$Timer.start()
+		$StatusLabel.text = 'Runing'
+	else:
+		$Timer.stop()
+		$StatusLabel.text = 'Stopped'
+		
